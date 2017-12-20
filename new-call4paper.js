94a7d6099cc4ae70b462ae178843fed025da6235
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-12-20 12:22:55 -0200

    novo call4paper via npm script

---
+++ new-call4paper.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const inquirer = require('inquirer'),
+    slug = require('slug'),
+    shell = require('shelljs');
+
+const questions = [
+    { name: 'name', type: 'input', message: 'Nome do Call4Paper:' },
+    { name: 'description', type: 'input', message: 'Descrição do Call4Paper:' },
+    { name: 'url', type: 'input', message: 'Url do Call4Paper:' },
+    { name: 'url_image', type: 'input', message: 'Url da Imagem do Evento:' },
+    { name: 'date_event', type: 'input', message: 'Data do Evento:' },
+    { name: 'submission_date', type: 'input', message: 'Data de Submissão:' },
+    { name: 'address_maps', type: 'input', message: 'Url Maps do Evento:' },
+    { name: 'address_name', type: 'input', message: 'Nome do Local do Evento:' },
+];
+inquirer.prompt(questions).then(answers => {
+    const eventName = answers.name,
+        fileName = slug(eventName.toLowerCase()),
+        filePath = 'src/events/' + fileName + '.html.md';
+
+    shell.touch(filePath);
+
+    const template = `---\ntype: call4papers\nname: ${answers.name}\ndescription: ${answers.description}\nurl: ${answers.url}\nurl_image: ${answers.url_image}\ndate_event: ${answers.date_event}\nsubmission_date: ${answers.submission_date}\naddress_maps: ${answers.address_maps}\naddress_name: ${answers.address_name}\n---`;
+
+    shell.ShellString(template).to(filePath);
+}).catch(function(err) {
+    console.log(err);
+});
